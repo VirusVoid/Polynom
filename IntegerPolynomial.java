e3f28fce5a7b735496df6e15606bc419ccd8a484
Александра Суменко <aleksa.sumenko@yandex.ru>
2017-03-26 01:05:46 +0300

    modified1

--- IntegerPolynomial.java
+++ IntegerPolynomial.java
@@ -1,33 +1,28 @@
-import java.lang.reflect.Array;
-import java.util.Arrays;
-
-/**
- * Created by aleks on 02.03.2017.
- */
-
 public class IntegerPolynomial {
     private int[] coef;
-    private int power;
 
-    public IntegerPolynomial(int[] coef, int power) {
-        if (power < 0) throw new IllegalArgumentException("Степень многочлена не может быть меньше нуля");
-        this.coef = coef;
-        this.power = getPower();
-    }
-
-    public int getPower() {
+    public int getDegree(int[] a) {
         int d = 0;
-        for (int i = 0; i < coef.length; i++)
-            if (coef[i] != 0)
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] != 0)
                 d = i;
+        }
         return d;
     }
+
+    public IntegerPolynomial(int[] coef) {
+        int[] coeff = new int[getDegree(coef) + 1];
+        if (coef.length == 0) throw new IllegalArgumentException("Многочлен не существует");
+        if (coef == null) throw new NullPointerException("Null");
+        System.arraycopy(coef, 0, coeff, 0, getDegree(coef) + 1);
+        this.coef = coeff;
+    }
+
 
     public boolean equal(IntegerPolynomial other) {
         IntegerPolynomial a = this;
         boolean k = true;
-        int n = 0;
-        if (a.power != other.power) {
+        if (getDegree(a.coef) != getDegree(other.coef)) {
             k = false;
         } else {
             for (int i = 0; i < a.coef.length; i++) {
@@ -40,8 +35,8 @@ public class IntegerPolynomial {
     public int meaning(int x) {
         int r = 0;
         int q = 1;
-        for (int i = 0; i <= power; i++) {
-            r += +coef[i] * q;
+        for (int i = 0; i < coef.length; i++) {
+            r += coef[i] * q;
             q *= x;
         }
         return r;
@@ -60,7 +55,6 @@ public class IntegerPolynomial {
             max = other.coef;
             min = a.coef;
         }
-        int pow = Math.max(a.power, other.power);
         int[] x = new int[maxLength];
         for (int i = 0; i < minLength; i++) {
             x[i] = min[i];
@@ -68,16 +62,16 @@ public class IntegerPolynomial {
         for (int i = 0; i < maxLength; i++) {
             x[i] += max[i];
         }
-        IntegerPolynomial result = new IntegerPolynomial(x, pow);
+        IntegerPolynomial result = new IntegerPolynomial(x);
         return result;
     }
+
 
     public IntegerPolynomial minus(IntegerPolynomial other) {
         IntegerPolynomial a = this;
         int maxLength = Math.max(a.coef.length, other.coef.length);
         int minLength = Math.min(a.coef.length, other.coef.length);
         int[] q = new int[maxLength];
-        int pow = Math.max(a.power, other.power);
         int[] min;
         if (a.coef.length >= other.coef.length) {
             min = other.coef;
@@ -88,60 +82,54 @@ public class IntegerPolynomial {
         for (int i = 0; i < minLength; i++) {
             s[i] = min[i];
         }
-        if (a.power < other.power) {
+        if (getDegree(a.coef) < getDegree(other.coef)) {
             for (int i = 0; i < maxLength; i++)
                 q[i] = s[i] - other.coef[i];
         } else {
             for (int i = 0; i < maxLength; i++)
                 q[i] = a.coef[i] - s[i];
         }
-        IntegerPolynomial result = new IntegerPolynomial(q, pow);
+        IntegerPolynomial result = new IntegerPolynomial(q);
         return result;
     }
 
     public IntegerPolynomial multiply(IntegerPolynomial other) {
         IntegerPolynomial a = this;
-        int[] res = new int[a.power + other.power + 1];
+        int[] res = new int[getDegree(a.coef) + getDegree(other.coef) + 1];
         for (int i = 0; i < a.coef.length; i++) {
             for (int j = 0; j < other.coef.length; j++) {
                 res[i + j] += a.coef[i] * other.coef[j];
             }
         }
-        IntegerPolynomial result = new IntegerPolynomial(res, a.power + other.power);
+        IntegerPolynomial result = new IntegerPolynomial(res);
         return result;
     }
 
-    public IntegerPolynomial multiply(int num, int p) {
+    private IntegerPolynomial multiply(int num, int p) {
         IntegerPolynomial a = this;
-        int[] x = new int[a.power + p + 1];
+        int[] x = new int[getDegree(a.coef) + p + 1];
         for (int i = 0; i < a.coef.length; i++)
             x[i + p] = a.coef[i] * num;
-        IntegerPolynomial result = new IntegerPolynomial(x, a.power + p);
+        IntegerPolynomial result = new IntegerPolynomial(x);
         return result;
     }
 
     public IntegerPolynomial divide(IntegerPolynomial other) {
-        if (other.power == 0 & other.coef[0] == 0)
+        if (getDegree(other.coef) == 0 & other.coef[0] == 0)
             throw new ArithmeticException("Знаменатель не может быть равен нулю");
         IntegerPolynomial a = this;
         IntegerPolynomial temp;
-        int[] x = new int[1];
-        int k;
-        if (power == other.power & a.coef[power] == other.coef[other.power]) {
-            x[0] = 1;
-        } else if (power < other.power) {
-            x[0] = 0;
-        } else {
-            x = new int[power - other.power + 1];
-            k = 0;
-            while (power - k >= other.power) {
-                x[power - other.power - k] = a.coef[power - k] / other.coef[other.power];
-                temp = other.multiply(x[power - other.power - k], power - other.power - k);
-                a = a.minus(temp);
-                k++;
-            }
+        int[] x = new int[a.coef.length];
+        int dividentdegree = getDegree(a.coef);
+        int divisordegree = getDegree(other.coef);
+        while (dividentdegree >= divisordegree) {
+            x[dividentdegree - divisordegree] = a.coef[dividentdegree] / other.coef[divisordegree];
+            temp = other.multiply(x[dividentdegree - divisordegree], dividentdegree - divisordegree);
+            a = a.minus(temp);
+            dividentdegree--;
+
         }
-        IntegerPolynomial result = new IntegerPolynomial(x, x.length - 1);
+        IntegerPolynomial result = new IntegerPolynomial(x);
         return result;
     }
 
@@ -158,9 +146,9 @@ public class IntegerPolynomial {
         if (obj == null) return false;
         if (obj instanceof IntegerPolynomial) {
             IntegerPolynomial other = (IntegerPolynomial) obj;
-            if (power != other.power) return false;
-            for (int i = 0; i <= power; i++)
-                if (coef[i] != other.coef[i]) return false;
+            if (getDegree(((IntegerPolynomial) obj).coef) != getDegree(other.coef)) return false;
+            for (int i = 0; i <= getDegree(other.coef); i++)
+                if (((IntegerPolynomial) obj).coef[i] != other.coef[i]) return false;
             return true;
         }
         return false;
@@ -168,10 +156,7 @@ public class IntegerPolynomial {
 
     @Override
     public int hashCode() {
-        int result = power;
-        for (int a : coef)
-            result = 37 * result + a;
-        return result;
+        return java.util.Arrays.hashCode(coef);
     }
 
     @Override
